0663f7b130ea5d0df035ba23040c34d71d1d1ea4
Sasha Uspenskaya <160512724+sashauspenskaya@users.noreply.github.com>
2024-03-27 16:46:21 -0400

    Update ForkingHomeworkRfile.R

--- ForkingHomeworkRfile.R
+++ ForkingHomeworkRfile.R
@@ -10,3 +10,5 @@ diamonds%>%
   filter(price > 7000 & price < 10000)%>%
   arrange(desc(carat))%>%
   slice(1:20)
+diamonds<- data.table(diamonds)
+diamonds[10000 > price & price >7000 & cut== "Premium", c("carat", "cut", "price"))]order(-carat)][c(1:20)]
